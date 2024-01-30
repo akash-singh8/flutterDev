63d8ef14423f7b648a36a74f0f566d35e29bd4e2
Akash Singh <developer.akash8@gmail.com>
2024-01-30 22:53:40 +0530

    calendar: set header & update styles

--- lib/calendar.dart
+++ lib/calendar.dart
@@ -10,6 +10,20 @@ class Calendar extends StatefulWidget {
 
 class _CalendarState extends State<Calendar> {
   DateTime today = DateTime.now();
+  Map<int, String> monthNames = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+  };
 
   void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
@@ -17,23 +31,58 @@ class _CalendarState extends State<Calendar> {
     });
   }
 
+  int getDaysInMonth(int year, int month) {
+    DateTime nextMonth = DateTime(year, month + 1, 1);
+    DateTime lastDayOfMonth = nextMonth.subtract(const Duration(days: 1));
+
+    return lastDayOfMonth.day;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TableCalendar(
-          focusedDay: today,
-          locale: "en_US",
-          rowHeight: 43,
-          headerStyle: const HeaderStyle(
-              formatButtonVisible: false, titleCentered: true),
-          availableGestures: AvailableGestures.all,
-          selectedDayPredicate: (day) => isSameDay(day, today),
-          firstDay: DateTime.utc(2020, 10, 16),
-          lastDay: DateTime.utc(2030, 10, 16),
-          onDaySelected: _onDaySelected,
-        ),
-      ],
+    String? month = monthNames[today.month];
+    int year = today.year;
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 14),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "$month $year",
+            style: const TextStyle(fontSize: 18, color: Colors.black54),
+          ),
+          const SizedBox(height: 14),
+          TableCalendar(
+            focusedDay: today,
+            locale: "en_US",
+            rowHeight: 48,
+            daysOfWeekHeight: 40,
+            headerVisible: false,
+            daysOfWeekStyle: const DaysOfWeekStyle(
+                weekdayStyle: TextStyle(color: Colors.black45),
+                weekendStyle: TextStyle(color: Colors.black45)),
+            selectedDayPredicate: (day) => isSameDay(day, today),
+            calendarStyle: const CalendarStyle(
+              // Customize the decoration for the selected day
+              selectedDecoration: BoxDecoration(
+                color: Color(0xFF218CAC), // Set the color for the selected day
+                shape: BoxShape.circle, // Shape of the decoration
+              ),
+              // Optionally, customize the decoration for today if you want to differentiate it from the selected day
+              todayDecoration: BoxDecoration(
+                color: Color.fromARGB(
+                    120, 33, 140, 172), // Set the color for the selected day
+                shape: BoxShape.circle,
+              ),
+            ),
+            firstDay: DateTime.now(),
+            lastDay: DateTime.utc(today.year, today.month,
+                getDaysInMonth(today.year, today.month)),
+            onDaySelected: _onDaySelected,
+          ),
+        ],
+      ),
     );
   }
 }
